268965c905c1711d65f6a147e02e42f18703fd0f
Dmitriy-Bitsenko <bitsenko@inbox.ru>
2024-04-05 15:58:36 +0500

    third commit

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -44,16 +44,15 @@ posts = [
 ]
 
 
-# Create your views here.
 def index(request):
     template = 'blog/index.html'
     context = {'posts': reversed(posts)}
     return render(request, template, context)
 
 
-def post_detail(request, id):
+def post_detail(request, pk):
     template = 'blog/detail.html'
-    context = {'post': posts[id]}
+    context = {'post': posts[pk]}
     return render(request, template, context)
 
 
